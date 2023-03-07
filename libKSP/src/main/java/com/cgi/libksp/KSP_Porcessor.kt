2a88f9b497cfa2241ca26efa84392e0f262d9a1c
Wolf, Hauke <hauke.wolf@cgi.com>
2023-03-07 10:09:12 +0100

    added code creation feature for testing

--- libKSP/src/main/java/com/cgi/libksp/KSP_Porcessor.kt
+++ libKSP/src/main/java/com/cgi/libksp/KSP_Porcessor.kt
@@ -11,8 +11,15 @@ class KSP_Porcessor(
     private val options: Map<String, String>
 ) : SymbolProcessor {
 
+    lateinit var file: OutputStream
+    var invoked = false
+
     operator fun OutputStream.plusAssign(str: String) {
         this.write(str.toByteArray())
+    }
+
+    fun emit(s: String, indent: String) {
+        file += ("$indent$s\n")
     }
 
 
@@ -23,23 +30,55 @@ class KSP_Porcessor(
         //tempchanges // aus den Symbols kann man sich strings usw vom AnotationAufruf Holen
         if (!symbols.iterator().hasNext()) return emptyList()
 
+        if (invoked) {
+            return emptyList()
+        }
+
+
+        file = codeGenerator.createNewFile(Dependencies(false), "", "TestProcessor", "log")
+        emit("TestProcessor: init($options)", "")
+
+        val javaFile = codeGenerator.createNewFile(Dependencies(false), "", "Generated", "java")
+        javaFile += ("class Generated {}")
+
+        val fileKt = codeGenerator.createNewFile(Dependencies(false), "", "HELLO", "java")
+        fileKt += ("public class HELLO{\n")
+        fileKt += ("public int foo() { return 1234; }\n")
+        fileKt += ("}")
+
+        val files = resolver.getAllFiles()
+        emit("TestProcessor: process()", "")
+
+        for (file in files) {
+            emit("TestProcessor: processing ${file.fileName}", "")
+
+        }
+
+/*
+
+
+        var counterForTest = 1
+
         val file: OutputStream = codeGenerator.createNewFile(
             dependencies = Dependencies(false, *resolver.getAllFiles().toList().toTypedArray()),
             packageName = "com.cgi.libKSPGenCode",
-            fileName = "GeneratedFunctionsSelf003"
+            fileName = "GeneratedFunctionsSelf" + counterForTest++.toString()
         )
 
         file += "package com.cgi.gen003\n"
 
+
         symbols.forEach { it.accept(Visitor(file), Unit) }
 
         file.close()
+*/
 
         //Konnte nicht verarbeitet werden
+        invoked = true
         return symbols.filterNot { it.validate() }.toList()
     }
-
-    inner class Visitor(private val file: OutputStream) : KSVisitorVoid() {
+//private val file: OutputStream // in Visitor
+    inner class Visitor() : KSVisitorVoid() {
 
         override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
         }
